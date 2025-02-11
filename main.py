493fd2cb667bac570e18c01547aab45acf5d8f0a
jun <jyunyaohmoto@gmail.com>
2025-02-11 10:29:05 +0900

    関数仮定義

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
 # お題「文字お越しアプリ」
 # TODO import 必要なライブラリ
+import whisper
 
-print("文字お越しを開始します")
+def main():
+    print("文字お越しを開始します")
 
-# TODO ファイル読み込み
+    # TODO ファイル読み込み
+    read_file()
 
-# TODO 文字お越し
+    # TODO 文字お越し
+    # TODO テキストファイルに書き込み
+    output_text()
 
-# TODO テキストファイルに書き込み
+    # TODO テキストファイルを保存
+    save_text()
 
-# TODO テキストファイルを保存
+    print("文字お越しが完了しました")
 
 
-print("文字お越しが完了しました")
+def read_file():
+    pass
+
+def output_text():
+    pass
+
+def save_text():
+    pass
+
+if __name__ == "__main__":
+    main()
